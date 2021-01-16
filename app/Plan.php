4c80501f3e56fe1a6b01bac257230686272b9713
Debian <bitnami@ip-172-26-7-111.ec2.internal>
2021-01-16 17:13:04 +0000

    aa

---
+++ app/Plan.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Plan extends Model
+{
+    protected $fillable = [
+        'name',
+        'price',
+        'duration',
+        'max_users',
+        'max_projects',
+        'description',
+    ];
+
+    public function arrDuration()
+    {
+        return [
+            'Unlimited' => 'Unlimited',
+            'Month' => 'Per Month',
+            'Year' => 'Per Year',
+        ];
+    }
+}
